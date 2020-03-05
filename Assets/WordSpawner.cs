e302981a66a4466cdefa263fb2f1780349b1611f
Michaela97 <31960647+Michaela97@users.noreply.github.com>
2020-03-05 18:32:46 +0100

    storing words done

--- Assets/WordSpawner.cs
+++ Assets/WordSpawner.cs
@@ -7,36 +7,92 @@ public class WordSpawner : MonoBehaviour
 {
     public GameObject word;
     private static float startTime;
-    private TextMesh tm;
+   // private TextMesh tm;
     private List<string> wordsHistory = new List<string>();
     private int position;
+    private bool gameOver;
+    private string curretWord;
+    private bool isWordSet;
+    
+    private List<GameObject> _gameObjects = new List<GameObject>();
     
     // Start is called before the first frame update
     void Start()
     {
-        startTime = Time.time;
+        //TEMPORRALY 
+        var word1 = CreateWord();
+        var word2 = CreateWord();
+        var word3 = CreateWord();
+        var word4 = CreateWord();
+        _gameObjects.Add(word1);
+        _gameObjects.Add(word2);
+        _gameObjects.Add(word3);
+        _gameObjects.Add(word4);
         
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpamWord();
-
-        if (wordsHistory.Count > 0)
-        {
-            var givenWord = wordsHistory[0]; //first word in this history list
+        //SpamWord();
+        var input = GetInput();
             
-            var input = GetInput();
-            
-            if (input != null)
+        if (input != null)
+        { 
+            if (!isWordSet)
             {
-                CheckWord(givenWord, input);
+                GetWordFromList(input, wordsHistory);
             }
+            CheckWord(input, curretWord);
         }
     }
 
-    private void CheckWord(string givenWord, string input)
+    //this is used to destroy specific object 
+    private GameObject GetGameObjectByName(string name)
+    {
+        foreach (var i in _gameObjects)
+        {
+            var tm = i.GetComponent<TextMesh>();
+            
+            if (tm.text == name)
+            {
+                Debug.Log("GetGameObject is returning object at position :" + i.transform.position);
+                return i;
+            }
+        }
+
+        Debug.Log("NO OBJECT BY NAME FOUND");
+        return null;
+    }
+    
+
+    private void SetWord(string givenWord)
+    {
+        if (!isWordSet)
+        {
+            curretWord = givenWord;
+            isWordSet = true;
+            Debug.Log("Set word: " + curretWord);
+        }
+    }
+
+    private void CheckWord(string input, string givenWord)
+    {
+        // if (wordsHistory.Count > 0)
+        // {
+            if (givenWord != null)
+            {
+                 CheckInput(givenWord,input);
+            }
+            else
+            {
+                Debug.Log("CheckWord : Given word is null");
+            }
+        // }
+    }
+
+    private void CheckInput(string givenWord, string input)
     {
         //check if position isn't bigger than word 
         var isCorrect = CheckInputAtPosition(input, givenWord, position);
@@ -44,23 +100,58 @@ public class WordSpawner : MonoBehaviour
         if (isCorrect)
         {
             Debug.Log("input: " + input + " was correct");
-            position++;
-            if (position == givenWord.Length)
-            {
-                Debug.Log("You typed word correctly");
-                //destroy object spam a new one 
-                position = 0; 
-            }
+            IncreasePosition(givenWord);
+
+            //RemoveLetter(GetGameObjectByName(givenWord));
         }
         else
         {
             position = 0;
             Debug.Log("input: " + input + " wasn't correct = GAME OVER" );
+            gameOver = true;
         }
-    } 
-    
-    
+    }
 
+    private void IncreasePosition(string givenWord)
+    {
+        position++;
+        
+         if (position == givenWord.Length)
+         {
+             position = 0;
+             wordsHistory.Remove(givenWord);
+             isWordSet = false;
+             Debug.Log("WORD DONE");
+
+             var objectToDestroy = GetGameObjectByName(curretWord);
+             _gameObjects.Remove(objectToDestroy);
+             Destroy(objectToDestroy);
+         }
+    }
+    
+    private void GetWordFromList(string character, List<string> strings)
+    {
+        foreach (var valueToCheck in strings)
+        {
+            if (valueToCheck.StartsWith(character))
+            {
+                SetWord(valueToCheck);
+                break;
+            }
+            
+            Debug.Log("NO MATCHES");
+            gameOver = true;
+        }
+    }
+
+    private void RemoveLetter(GameObject gameObject)
+    {
+       var textMesh = gameObject.GetComponent<TextMesh>();
+       textMesh.text =  textMesh.text.Remove(0, 1);
+       //curretWord.Remove(0, 1);
+
+    }
+    
     private string GetInput()
     {
         var input = Input.inputString;
@@ -76,7 +167,7 @@ public class WordSpawner : MonoBehaviour
     void SpamWord()
     {
         var currentTime =  Time.time;
-        if (currentTime - startTime > 4f)
+        if (currentTime - startTime > 4f && gameOver == false)
         {
             var newWord = CreateWord();
             startTime = currentTime;
@@ -96,7 +187,7 @@ public class WordSpawner : MonoBehaviour
         GameObject newWord = Instantiate(word);
         GetRandomPosition(newWord);
         
-        tm = newWord.GetComponent<TextMesh>();
+        var tm = newWord.GetComponent<TextMesh>();
         var text = GetRandomString(Random.Range(5,9));
         tm.text = text;
         
